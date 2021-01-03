3c30e614ec38325eab99adf19044c67ef9b76e2c
Marcos Sartori <marcos.sartori@acad.pucrs.br>
2021-01-03 16:22:33 -0200

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,17 @@
 //! # immutable_string - Immutable Single Instance Strings for Rust
 //! This crate provides a global table for immutable string implemented using Arc<str>.
 //! It avoids duplication of strings in memory when parsing large files with multiple instances of the same sequence of characters.
-//! 
+//!
 //! A global shared WeakHashSet keeps a reference for every live ImmutableString.
 //! A ImmutableString can have multiple owners.
 //! Once every owner drops a ImmutableString, it is lazily removed from the WeakHashSet and dealocated.
-//! 
+//!
 //! The globally shared WeakHashSet is protected by a RwLock which allows multiple concurrent readers but guarantees that any writer has exclusive access.
 //! When instantiating an ImmutableString, the constructor first acquires a reader to check whether the value is already present in the map.
 //! If not, it forgoes the reader lock and attempt to acquire the exclusive writer lock.
 //! Once it has exclusive writer access, it checks again if the string is not present in the map.
 //! Then, it allocates the string and store a weak copy in the hashmap.
-//! 
+//!
 //! The globally shared WeakHashSet may present a performance bottleneck and in the future should be replaced by a distribuited hashmap.
 
 use lazy_static::lazy_static;
@@ -33,7 +33,6 @@ pub struct ImmutableString(Arc<str>);
 
 impl ImmutableString {
     /// Returns the number of ImmutableStrings referencing the same data.
-    /// This is `Arc::strong_count` of the internal Arc.
     ///
     /// ```
     /// use immutable_string::*;
@@ -42,7 +41,7 @@ impl ImmutableString {
     /// let a0 = ImmutableString::from("a");
     /// let a1 = a.clone();
     /// let b  = ImmutableString::from("b");
-    /// 
+    ///
     /// assert_eq!(a.use_count(), 3);
     /// assert_eq!(a0.use_count(), 3);
     /// assert_eq!(a1.use_count(), 3);
